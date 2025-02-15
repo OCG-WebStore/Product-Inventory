b7bb7bfc7826f188bef90ddd96abd821b20cb918
TwelveWet <62149776+12wet@users.noreply.github.com>
2025-02-15 19:43:10 +0100

    Category implementation

--- app/models/Category.scala
+++ app/models/Category.scala
@@ -2,7 +2,9 @@ package models
 
 import slick.jdbc.PostgresProfile.api._
 
-case class Category(productType: String)
+trait Category {
+  def stringValue: String
+}
 
 object Category {
   val HOODIES = "hoodies"
@@ -12,10 +14,34 @@ object Category {
 
   implicit val categoryColumnType: BaseColumnType[Category] =
     MappedColumnType.base[Category,String](
-      cat => cat.productType,
+      cat => cat.stringValue,
       str => Category(str)
     )
 
+  def apply(str: String): Category = str match {
+    case HOODIES => Hoodies()
+    case TSHIRTS => TShirts()
+    case TROUSERS => Trousers()
+    case GLOVES => Gloves()
+    case _ => throw new IllegalArgumentException("Invalid category")
+  }
+
   import play.api.libs.json.{Format, Json}
   implicit val categoryFormat: Format[Category] = Json.format[Category]
+}
+
+case class Hoodies() extends Category {
+  override def stringValue: String = Category.HOODIES
+}
+
+case class TShirts() extends Category {
+  override def stringValue: String = Category.TSHIRTS
+}
+
+case class Trousers() extends Category {
+  override def stringValue: String = Category.TROUSERS
+}
+
+case class Gloves() extends Category {
+  override def stringValue: String = Category.GLOVES
 }
