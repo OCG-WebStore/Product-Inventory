f4b08049489bf31653b1d85a6e8fefda4725973c
TwelveWet <62149776+12wet@users.noreply.github.com>
2025-02-21 04:18:04 +0100

    env vars for application conf and configuration class

---
+++ app/utils/OCGConfiguration.scala
@@ -0,0 +1,42 @@
+package utils
+
+import play.api.Configuration
+
+import javax.inject.{Inject, Singleton}
+
+
+@Singleton
+class OCGConfiguration @Inject()(
+                                 config: Configuration
+                                 ){
+
+  object Redis {
+    final val host          = config.getOptional[String]      ("redis.host")                   .getOrElse("localhost")
+    final val port          = config.getOptional[Int]         ("redis.port")                   .getOrElse(6379)
+    final val password      = config.getOptional[String]      ("redis.password")
+    final val ttl           = config.getOptional[Int]         ("ttl")                          .getOrElse(36000)
+  }
+
+  object User {
+
+    object Context {
+      final val expiresAt   = config.getOptional[Long]       ("user.context.expiresAt")       .getOrElse(3600L)
+    }
+
+    final val authEnabled   = config.getOptional[Boolean]    ("user.authentication.enabled")  .getOrElse(true)
+  }
+
+  object Play {
+
+    object Http {
+      final val secretKey   = config.getOptional[String]      ("play.http.secret.key")        .getOrElse("")
+    }
+  }
+
+  object App {
+
+    object Security {
+      final val hmacSecret  = config.getOptional[String]      ("app.security.hmac.secret")    .getOrElse("")
+    }
+  }
+}
